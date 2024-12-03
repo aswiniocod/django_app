bce347bb03d0b82398c315233ac03d3f3c8cb947
aswiniocod <aswin@iocod.com>
2024-12-03 04:49:55 +0000

    Changes to be committed: 	modified:   appspec.yml 	modified:   scripts/run_migrations.sh

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -14,6 +14,9 @@ echo "Requirements installed successfully"
 python manage.py migrate --noinput || exit 1  # Exit if migration fails
 echo "Database migration completed successfully"
 
+# Create the superuser using python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('aswin', 'aswin@iocod.com', 'admin@123')" | python3 manage.py shell
+
 # Restart the app service (replace 'app.service' with your actual service name)
 sudo systemctl restart app.service || exit 1  # Exit if service restart fails
 echo "App service restarted successfully"
