f1ca725a1a866e5ae77d7c9796cd3976efacae71
aswiniocod <aswin@iocod.com>
2024-12-04 10:20:05 +0000

    Changes to be committed: 	modified:   web/models.py

--- web/models.py
+++ web/models.py
@@ -8,6 +8,7 @@ class TestModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     designation = models.CharField(max_length=100, blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
