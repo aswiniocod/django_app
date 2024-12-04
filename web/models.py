7b993ae4200f292a8eeacef126f4586f19989bec
aswiniocod <aswin@iocod.com>
2024-12-04 05:51:27 +0000

    Changes to be committed: 	modified:   .github/workflows/main.yml 	modified:   web/models.py

--- web/models.py
+++ web/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class TestModel(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    designation = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
