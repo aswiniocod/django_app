529ba3b432bb890db5cfd7f1334e99c6b658a925
aswiniocod <aswin@iocod.com>
2024-12-04 07:08:22 +0000

    Changes to be committed: 	modified:   scripts/after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,17 +3,14 @@
 echo "Starting after_install script..."
 
 # Step 1: Activate the virtual environment (if you have one)
-cd /home/ubuntu/django_app
-
 echo "Activating virtual environment..."
 source /home/ubuntu/venv/bin/activate
 
 # Step 2: Install dependencies (if requirements.txt has changed or if you're setting it up for the first time)
 echo "Installing Python dependencies..."
-#pip install -r /home/ubuntu/django_app/requirements.txt
 pip install -r /home/ubuntu/django_app/requirements.txt
 
-# Step 2: Run makemigrations
+# Step 3: Run makemigrations
 echo "Running makemigrations..." >> /tmp/deployment.log
 /home/ubuntu/venv/bin/python /home/ubuntu/django_app/manage.py makemigrations >> /tmp/deployment.log 2>&1
 
@@ -23,7 +20,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 3: Run migrate
+# Step 4: Run migrate
 echo "Running migrate..." >> /tmp/deployment.log
 /home/ubuntu/venv/bin/python /home/ubuntu/django_app/manage.py migrate >> /tmp/deployment.log 2>&1
 
@@ -33,20 +30,27 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 4: Deactivate the virtual environment
-deactivate
+# Step 5: Create superuser using python manage.py shell
+echo "Creating superuser..." >> /tmp/deployment.log
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('aswin', 'aswin@iocod.com', 'admin@123')" | /home/ubuntu/venv/bin/python /home/ubuntu/django_app/manage.py shell >> /tmp/deployment.log 2>&1
 
-# Create the superuser using python manage.py shell
-sudo echo "from django.contrib.auth.models import User; User.objects.create_superuser('aswin', 'aswin@iocod.com', 'admin@123')" | python3 manage.py shell
+if [ $? -ne 0 ]; then
+    echo "Superuser creation failed." >> /tmp/deployment.log
+    deactivate
+    exit 1
+fi
 
-# Step 1: Restart the application service
+# Step 6: Restart the application service
 echo "Restarting app service..." >> /tmp/deployment.log
-sudo systemctl start app.service >> /tmp/deployment.log 2>&1
 sudo systemctl restart app.service >> /tmp/deployment.log 2>&1
 
-# Step 2: Apply database migrations after restarting the service (optional)
-# echo "Running Django migrations..." >> /tmp/deployment.log
-# source /home/ubuntu/django_app/venv/bin/activate
-# python /home/ubuntu/django_app/manage.py migrate >> /tmp/deployment.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Failed to restart app service." >> /tmp/deployment.log
+    deactivate
+    exit 1
+fi
+
+# Step 7: Deactivate the virtual environment
+deactivate
 
 echo "Completed after_install script" >> /tmp/deployment.log
